5c409e94595626353ffc7f53ef0c5aa44945bdd8
jacobkwiat1 <jacobkwiat@gmail.com>
2025-09-26 14:53:05 -0700

    got the encryption and decryption working

--- Encrypter.cpp
+++ Encrypter.cpp
@@ -18,24 +18,39 @@ int* Encrypter::encrypt(std::string input, std::string key) {
     out[1] = input.at(0);
     //plan is letter ascii - previous letter ascii%30 - key value ascii = new current letter
     //hoping usually won't have ascii values below 32
-    for(auto i=static_cast<int>(input.length())-1; i > 1;i--) {
-        std::cout << "num: " << ((input.at(i) - input.at(i-1)/*%30*/ + key.at(i%key.length()))/*%224*/ + 31) << std::endl;
+    for(auto i=static_cast<int>(input.length()); i > 1;i--) {
+        std::cout << i << ": key: " << static_cast<int>(key.at(i%key.length())) << ", num: " << ((input.at(i-1) - input.at(i-2)/*%30*/ + key.at(i%key.length()))/*%224*/ + 31) << std::endl;
         //out[i] = static_cast<char>((input.at(i) - input.at(i-1)/*%30*/ + key.at(i%key.length()))/*%224*/ + 31);
-        out[i] = (input.at(i) - input.at(i-1)/*%30*/ + key.at(i%key.length()))/*%224*/ + 31;
+        out[i] = (input.at(i-1) - input.at(i-2)/*%30*/ + key.at(i%key.length()))/*%224*/ + 31;
     }
     out[0] = input.length();
     return out;
 }
 
-std::string Encrypter::decrypt(std::string input, std::string key) {
-    std::string out = input;
-    std::cout << "decoding\n" << out << std::endl;
+std::string Encrypter::decrypt(std::string inputF, std::string keyF) {
+    std::string out;
+    std::ifstream inputFile(inputF);
+    int size;
+    inputFile >> size;
+    int* input = new int[size];
+    for(int i = 0; i < size; i++){
+        inputFile >> input[i];
+        std::cout << i << ": " << input[i] << std::endl;
+    }
+    inputFile.close();
+    inputFile.open(keyF);
+    std::string key;
+    std::getline(inputFile, key);
+    inputFile.close();
 
-    for(auto i = 1; i < input.length(); i++) {
-        out[i] = static_cast<char>(out.at(i) + out.at(i-1) - key.at(i%key.length()) - 31);
-        std::cout << "key: " << key.at(i%key.length()) << ", encoded: " << input.at(i) << ", previous: " << out.at(i-1) << ";;;; next: " << out.at((i+1)%input.length()) << ", nextKey: " << key.at(((i+1)%key.length())) << "nextOut: " << (out.at((i+1)%out.length()) + out.at(i) - key.at((i+1)%key.length()) - 31) << std::endl;
+    //std::cout << "decoding\n" << out << std::endl;
+    out.push_back(static_cast<char>(input[0]));
+    for(auto i = 1; i < size; i++) {
+        out.push_back(static_cast<char>(input[i] + out.at(i-1) - key.at((i+1)%key.length()) - 31));
+        std::cout << i << ": key: " << static_cast<int>(key.at((i+1)%key.length())) << ", encoded: " << input[i] << ", previous: " << out.at(i-1) << std::endl;
 
     }
+    delete [] input;
     return out;
 }
 
--- main.cpp
+++ main.cpp
@@ -5,33 +5,23 @@
 #include "Encrypter.hpp"
 int main() {
     std::cout << "enter something\n";
+    std::string in;
     std::getline(std::cin, in);
     Encrypter thing;
     // std::string out = thing.encrypt(in);
     int* out = thing.encrypt(in);
     std::ofstream outputFile("encrypted.txt");
-    for(int i = 0; i < in.length(); i++) {
+    for(int i = 0; i < in.length()+1; i++) {
         std::cout << "something";
         outputFile << out[i] << " ";
     }
-    delete [] out;
     outputFile.close();
+    delete [] out;
     std::cout << "moving onto decoding\n";
-    std::ifstream inputFile("encrypted.txt");
-    int temp;
-    int size;
-    int i = 0;
-    inputFile >> size;
-    int* in = new int[size];
-    for(int i = 0; i < size; i++){
-        inputFile >> in[i];
-    }
-    inputFile.close();
-    inputFile.open("key.txt");
-    std::string out = key;
-    std::getline(inputFile, key);
-    inputFile.close();
-    std::string output = thing.decrypt(in, key);
-    std::cout << std::endl << out;
+    std::string iFile = "encrypted.txt";
+    std::string kFile = "key.txt";
+
+    std::string output = thing.decrypt(iFile, kFile);
+    std::cout << output;
     return 0;
 }
